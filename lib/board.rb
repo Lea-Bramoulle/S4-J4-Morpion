16270b5c71c9ed72ebb6b8dba7ba0451e61e692c
Lea-Brml <l.bramoulle@hotmail.fr>
2019-10-17 21:49:05 +0200

    youpiheyyyyyy ca m'a souleyyyyyyy

--- lib/board.rb
+++ lib/board.rb
@@ -2,10 +2,13 @@ class Board
 
 	def initialize
 	  @valeur = []
-  	  tab_position = ["A1","A2","A3","B1","B2","B3","C1","C2","C3"]
-			@tab_win = ["a","b","c","d","e","f","g","h","i"]                   # dès qu'une valeur X ou O est placée, elle est s'injecte dans cetableau et remplace la valeur initiale (nous permet de tester les égalité et donc déterminer s'il y a victoire ou défaite)
 
-	  i = 0                #au début de la partie on atribu des " " pour que toutesles cases soient vides avant d'être remplies / remplacées par des X ou O
+		tab_position = ["A1","A2","A3","B1","B2","B3","C1","C2","C3"]
+		@tab_win = ["a","b","c","d","e","f","g","h","i"]                   # dès qu'une valeur X ou O est placée, elle est s'injecte dans cetableau et remplace la valeur initiale (nous permet de tester les égalité et donc déterminer s'il y a victoire ou défaite)
+
+
+	  i = 0  		              #au début de la partie on atribu des " " pour que toutesles cases soient vides avant d'être remplies / remplacées par des X ou O
+
 	  while i < 10
 	    @valeur[i] = " "
 	    i = i + 1
@@ -46,27 +49,27 @@ class Board
 	    puts "Sur quelle case souhaites-tu jouer frr ? (A1 à C3)"
 	    print "> "
 	    reponse = gets.chomp
-	    z = 1
 
-	   tab_position.each do |item|
-  	     if reponse != item
-		z = z + 1
+		 z = 1
+
+	   tab_position.each do |item|									#on va passer sur chacune des valeurs du tableau contenant les positions du bpard (de A1 à C3) pour vérifier si une de ces valeurs est égale au gets.chomp où le joueur entre la case où il souhaite positionner son symbole X ou O
+  	     if reponse != item												#Si la valeur entrée dans le gets.chomp ne correspond à aucune des cases du board, on retourne en haut de la boucle
+					 z = z + 1
   	     else
-		system('clear')
-		if @valeur[z] == " "
-		  if @tour_de_jeu % 2 == 0
-		    @valeur[z] = "X"
-				@tab_win[z] = "X"
-		  else
-		    @valeur[z] = "O"
-				@tab_win[z] = "O"
-		  end
-		  @tour_de_jeu = @tour_de_jeu + 1
-		else
-
-		    puts "La place est deja prise !"
-	        end
-       	     end
+					system('clear')													#pour raffraichir la fenetre du terminal
+					if @valeur[z] == " "
+					  if @tour_de_jeu % 2 == 0							#si on est à un tour de jeu impaire (tour numéro 1 3 5 7 (on initialise z à 1)) alors on considère que c'est le joueur 1 qui joue, donc qui a le symbole X sur le board
+					    @valeur[z] = "X"
+							@tab_win[z] = "X"
+					  else																	#else, le module est différent de 0 donc tour de boucle impaire donc joueur 2 qui joue avec le symbole O
+					    @valeur[z] = "O"
+							@tab_win[z] = "O"
+					  end
+					  @tour_de_jeu = @tour_de_jeu + 1
+					else
+					  puts "La place est deja prise !"
+				  end
+			 end
 
 	  end
 
